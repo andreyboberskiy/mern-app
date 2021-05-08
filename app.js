b414ad27e14502d33b42f056e6713c41a50d6669
kortez-1337 <kortezsobaken@gmail.com>
2021-05-08 15:08:56 +0300

    Setting git for cliend

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ const app = express();
 app.use(express.json({ extended: true }));
 
 app.use("/api/auth", require("./routes/auth.routes"));
-
 const PORT = config.get("port") || 5000;
 
 async function start() {
